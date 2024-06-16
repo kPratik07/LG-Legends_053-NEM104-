9d20eb9cd191278c1d8126209332abbb9426380f
sourabh7322 <sourabhrawat77200@gmail.com>
2024-06-16 17:36:42 +0530

    login part complete

--- Server/controllers/auth.js
+++ Server/controllers/auth.js
@@ -77,7 +77,7 @@ exports.signup = async (req, res) => {
             accountType, contactNumber, otp } = req.body;
 
         // validation
-        if (!firstName || !lastName || !email || !password || !confirmPassword || !accountType||!otp) {
+        if (!firstName || !lastName || !email || !password || !confirmPassword || !accountType || !otp) {
             return res.status(401).json({
                 success: false,
                 message: 'All fields are required..!'
