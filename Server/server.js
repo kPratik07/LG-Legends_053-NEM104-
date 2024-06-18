7844c2a7146269c9a5db7ea10d8306b78bb70668
Sourabh Rawat <158592811+sourabh7322@users.noreply.github.com>
2024-06-18 14:38:56 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const cors=require('cors');
 const mailSender = require('./utils/mailSender.js')
-mailSender("adityasinghuo@gmail.com", "otp", "895");
+mailSender("kumar.rajesh5656@gmail.com", "otp", "895");
 
 // packages
 const fileUpload = require('express-fileupload');
