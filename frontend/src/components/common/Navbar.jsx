9dfad556657992952deb3052f70609888aa4e8f1
Sourabh Rawat <158592811+sourabh7322@users.noreply.github.com>
2024-06-18 15:05:32 +0530

    Update Navbar.jsx

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link, matchPath, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import { NavbarLinks } from "../../../data/navbar-links";
-import knowledgeKnookLogo from "../../assets/Logo/Knowledge-Knook.png";
+import knowledgeKnookLogo from "../../assets/Logo/Logo-Full-Light.png";
 import { fetchCourseCategories } from "./../../services/operations/courseDetailsAPI";
 
 import ProfileDropDown from "../core/Auth/ProfileDropDown";
